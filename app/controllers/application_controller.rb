5a7d0d99e734f3c0273364d19cb9ad9799204186
Bhargav-Thummar <bhargav.thummar810@gmail.com>
2024-03-29 16:48:32 +0530

    Feat: Add module to handle errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::API
+  # catch an exceptions
+  include Errors::ErrorHandler
+
   def current_game
     @current_game = Game.find(session[:game_id])
   end
